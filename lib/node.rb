afcbd095b3eaee33e4791d48d21c6c8b1ea2df21
Дмитрий Ануфриев <dmitryanufriev@gmail.com>
2020-06-15 17:32:34 +0000

    Rename field and ctor arg.

--- lib/node.rb
+++ lib/node.rb
@@ -6,8 +6,8 @@ require_relative 'common_prefix'
 class Trie
   # Node which contains prefix and subnodes with suffixes.
   class Node
-    def initialize(word, values, descendants = {})
-      @word = word
+    def initialize(prefix, values, descendants = {})
+      @prefix = prefix
       @values = values
       @descendants = descendants
     end
@@ -21,29 +21,29 @@ class Trie
     def add(word, value)
       raise ArgumentError, "Word can't be nil or empty" if word.nil? || word.blank?
 
-      common_prefix = CommonPrefix.new(@word, word).max
-      raise ArgumentError, "Node '#{@word}' has no common prefix with word '#{word}'" if common_prefix.blank?
+      common_prefix = CommonPrefix.new(@prefix, word).max
+      raise ArgumentError, "Node '#{@prefix}' has no common prefix with word '#{word}'" if common_prefix.blank?
 
-      # Case A: same words -> @word:hello - word:hello
-      if common_prefix.length == @word.length && common_prefix.length == word.length
-        return Node.new @word, @values.push(value), @descendants
+      # Case A: same words -> @prefix:hello - word:hello
+      if common_prefix.length == @prefix.length && common_prefix.length == word.length
+        return Node.new @prefix, @values.push(value), @descendants
       end
 
-      suffix_a = @word.suffix(common_prefix)
+      suffix_a = @prefix.suffix(common_prefix)
       suffix_b = word.suffix(common_prefix)
-      # Case B: suffix of @word is empty -> @word:hell - word:hello
+      # Case B: suffix of @prefix is empty -> @prefix:hell - word:hello
       if suffix_a.empty?
         key = suffix_b[0]
         descendants = {}.merge @descendants
         descendants[key] = descendants.key?(key) ? descendants[key].add(suffix_b, value) : Node.new(suffix_b, [value])
         return Node.new common_prefix, @values, descendants
       end
-      # Case C: suffix of word is empty -> @word:hello - word:hell
+      # Case C: suffix of word is empty -> @prefix:hello - word:hell
       if suffix_b.empty?
         descendants = { suffix_a[0] => Node.new(suffix_a, @values, {}.merge(@descendants)) }
         return Node.new common_prefix, [value], descendants
       end
-      # Case D: suffixes are not empty -> @word:hell -> word:help
+      # Case D: suffixes are not empty -> @prefix:hell -> word:help
       descendants = { suffix_a[0] => Node.new(suffix_a, @values, {}.merge(@descendants)) }
       descendants[suffix_b[0]] = Node.new(suffix_b, [value])
       Node.new common_prefix, [], descendants
@@ -56,7 +56,7 @@ class Trie
     # @return enumerable of values or empty if no words matched to 'word' parameter found.
     def values(word, match = :starts_with)
       Enumerator.new do |yld|
-        if @word == word || match == :starts_with && @word.start_with?(word)
+        if @prefix == word || match == :starts_with && @prefix.start_with?(word)
           @values.each { |value| yld << value }
           if match == :starts_with
             @descendants.values.each do |descendant|
@@ -64,7 +64,7 @@ class Trie
             end
           end
         else
-          suffix = word.suffix(CommonPrefix.new(@word, word).max)
+          suffix = word.suffix(CommonPrefix.new(@prefix, word).max)
           key = suffix[0]
           @descendants[key].values(suffix, match).each { |value| yld << value } if @descendants.key?(key)
         end
@@ -76,7 +76,7 @@ class Trie
     # @param [Integer] depth is a depth of the node in Trie. Default value is 0.
     # @param [Proc] block is a block which will be invoked with node's depth, node's word and node's values.
     def traverse(depth = 0, &block)
-      block.call(depth, @word, @values)
+      block.call(depth, @prefix, @values)
       @descendants.values.each { |node| node.traverse(depth + 1, &block) }
     end
   end
