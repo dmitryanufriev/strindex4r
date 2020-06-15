7bfc6e9d6c5821ef1cc8f9cf0ef195e2a2d7721c
Дмитрий Ануфриев <dmitryanufriev@gmail.com>
2020-06-15 17:10:59 +0000

    Add documentation comments

--- lib/string.rb
+++ lib/string.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
+# String class extensions
 class String
+  # Returns suffix for given prefix.
+  # For example: for word 'hello' and prefix 'hel' suffix will be 'lo'.
   def suffix(prefix)
     delete_prefix prefix if start_with? prefix
   end
 
+  # Returns true if string is empty or contains spaces only.
   def blank?
     strip.empty?
   end
