f77ce1207c61e68fdb18af522ac34c3403713cb0
Дмитрий Ануфриев <dmitryanufriev@gmail.com>
2020-06-16 17:28:21 +0000

    Made method 'max' shorter.

--- lib/common_prefix.rb
+++ lib/common_prefix.rb
@@ -2,7 +2,6 @@
 
 # Represents common prefix between two words.
 class CommonPrefix
-
   def initialize(word_a, word_b, ignore_case = true)
     @word_a = word_a
     @word_b = word_b
@@ -14,15 +13,12 @@ class CommonPrefix
   #
   # @return string, which is the longest common prefix between two words or empty string if no common prefix.
   def max
-    word_length = [@word_a.length, @word_b.length].min - 1
-    word_length.downto(0) do |hi|
+    min_word_length = [@word_a.length, @word_b.length].min - 1
+    min_word_length.downto(0) do |hi|
       prefix_a = @word_a[0..hi]
       prefix_b = @word_b[0..hi]
-      if @ignore_case
-        return prefix_a if prefix_a.casecmp(prefix_b).zero?
-      else
-        return prefix_a if prefix_a == prefix_b
-      end
+      return prefix_a if @ignore_case && prefix_a.casecmp(prefix_b).zero?
+      return prefix_a if prefix_a == prefix_b
     end
     ''
   end
